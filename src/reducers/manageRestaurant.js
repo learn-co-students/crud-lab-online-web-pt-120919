12d5e3da282dfb7b028645ef014565179de68274
Mely07 <damely.tineo07@gmail.com>
2020-09-22 00:21:15 -0400

    Done.

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,5 +1,50 @@
 import cuid from 'cuid';
+import { combineReducers } from "redux";
 
-export default function manageRestaurants(state, action) {
+const rootReducer = combineReducers({
+    restaurants: restaurantsReducer,
+    reviews: reviewsReducer
+});
 
+export default rootReducer;
+
+function restaurantsReducer(state = [], action) {
+    switch (action.type) {
+        case 'ADD_RESTAURANT':
+            const restaurant = {
+                id: cuid(),
+                text: action.text
+            }
+            return [...state, restaurant];
+
+        case 'DELETE_RESTAURANT':
+            return state.filter(restaurant => restaurant.id !== action.id);
+
+        default:
+            return state;
+    }
+}
+
+function reviewsReducer(state = [], action) {
+
+    switch (action.type) {
+
+        case 'ADD_REVIEW':
+            // const review = {
+            //     id: cuid(),
+            //     restaurantId: action.review.id,
+            //     text: action.review.text
+            // }
+            // return [...state, review];
+
+            action.review.id = cuid()
+
+            return [...state, action.review]
+
+        case 'DELETE_REVIEW':
+            return state.filter(review => review.id !== action.id);
+
+        default:
+            return state;
+    }
 }
