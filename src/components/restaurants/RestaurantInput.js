fa24bc8f022f79da014ae6de6b8945a3f064d01e
MilanP5G <milan_parmar@hotmail.co.uk>
2020-09-24 16:12:26 +0100

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+    state = {
+      text: ''
+    }
+
+
+  handleSubmit = event => {
+    event.preventDefault()
+    this.props.addRestaurant(this.state.text)
+    this.setState({
+      text: ''
+    })
+  }
+
+  handleChange(event) {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+      <label>Restaurant Name:</label>
+       <form onSubmit={(event) => this.handleSubmit(event)}>
+        <input
+          type='text'
+          onChange={((event) => this.handleChange(event))}
+          value={this.state.text}
+        />
+        <input type='submit' />
+       </form>
       </div>
     );
   }
 };
+
+
 
 export default RestaurantInput;
--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant'
 
 class Restaurants extends Component {
   render() {
     return(
       <ul>
-        Restaurants Component
+        {this.props.restaurants.map(restaurant => {return <li>{<Restaurant restaurant={restaurant} key={restaurant.id} id={restaurant.id} deleteRestaurant={this.props.deleteRestaurant}/>}</li>})}
       </ul>
     );
   }
